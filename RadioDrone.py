10f6480d97684c135688da29e06d24269dc86d2a
Guy John <guy@notesandvolts.com>
2011-01-14 02:58:32 -0800

    Renamed some objects, moved methods in pureData object around for clarity, added better output logging, improved kill_old_patch method so that it now turns DSP for the subpatch off before closing it

--- RadioDrone.py
+++ RadioDrone.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 
 #Import Modules
@@ -35,85 +36,43 @@ class LoggingObj():
         
         
         
-class MasterPD(Pd):
+class PureData(Pd):
         
-    def __init__(self, comPort=30320, streamPort=30310, foreground=False):
+    def __init__(self, comPort=30320, streamPort=30310, debug=False):
         self.patchName   = 'masterPatch.pd'
         self.streamPort  = streamPort
-        self.name        = 'masterPD'
+        
         self.activePatch = 2
         self.oldPatch    = 1
-        self.fadeTime    = 10
+        
         self.patches     = {}
         self.patchNames  = {}
+        
         self.playTime    = 30
-        self.foreground  = foreground
+        self.debug       = debug
 
-        gui              = self.foreground
+        gui              = self.debug
         extras           = "-alsa"
         
         Pd.__init__(self, comPort, gui, self.patchName, extra=extras)
         
-        
-    def streaming_control(self, streamStatus):
-        #send a message to the streaming controls in the master patch
-        message = [stream, control, streamStatus]
-        self.Send(message)
-        
-    def crossfade(self):
-        #fade across to new active patch
-        newPatch = 'patch' + str(self.activePatch)
-        oldPatch = 'patch' + str(self.oldPatch)
-
-        newmessage = [newPatch, 'volume', 1]
-        oldmessage = [oldPatch, 'volume', 0]
-
-        self.Send(newmessage)
-        self.Send(oldmessage)
-        self.pause(10)
-        
+        logFile.log(self.args)
+    
     def pause(self, pauseLength):
         #pause for a specified number of seconds
         #will keep updating the master patch and sub patches
         #so that network communication still works
-        print "pausing for a bit"
+        logFile.log("Pausing for %i seconds" % pauseLength)
         start = time()
         while time() - start < pauseLength:
             self.Update()
-        
-    def create_new_patch(self):
-        #get a random patch from the patch folder
-        patchName, patchPath = self.get_random_patch()
-        self.patchNames[self.activePatch] = patchName
-        #create new patch in the active patch slot
-        self.Send(['open', 'path', patchPath])
-        self.Send(['open', 'patch', patchName])
-        self.pause(1)
-        
-    def get_random_patch(self):
-        #get a random patch from the patch directory
-        #currently just returns the test patch
-        #will need to figure out how this is going to work
-        fileName = 'test%i.pd' % self.activePatch
-        patchInfo = (fileName, '/home/guy/gitrepositories/Radio-PD/patches')
-        return patchInfo
-        #patchList = os.listdir(patchDir)
-        #patch = random.choose(patchList)
-        
-    def stop_old_patch(self):
-        #disconnect old patch from master patch and then del the object
-        if self.oldPatch in self.patches.keys():
-            patchName = self.patchNames[self.oldPatch]
-            message = ['close', patchName]
-            self.Send(message)
-            del(self.patches[self.oldPatch])
-        pass
     
-    def activate_patch(self):
-        patch = 'patch' + str(self.activePatch)
-        message = [patch, 'dsp', 1]
+    def streaming_control(self, streamControl):
+        #send a message to the streaming controls in the master patch
+        logFile.log("Streaming control, %s" % streamControl)
+        message = ["stream", "control", streamControl]
         self.Send(message)
-
+    
     def switch_patch(self):
         #change the active patch number
         #real scrappy, needs to be neater
@@ -123,28 +82,82 @@ class MasterPD(Pd):
         elif self.activePatch == 2:
             self.activePatch = 1
             self.oldPatch    = 2
-   
+    
+    def create_new_patch(self):
+        #get a random patch from the patch folder
+        patchName, patchPath = self.get_random_patch()
+        self.patchNames[self.activePatch] = patchName
+        #create new patch in the active patch slot
+        logFile.log("Opening new patch: %s in %s" % (patchName, patchPath)
+        self.Send(['open', 'path', patchPath])
+        self.Send(['open', 'patch', patchName])
+        self.pause(1)
+    
+    def get_random_patch(self):
+        #get a random patch from the patch directory
+        #currently just returns the test patch
+        #will need to figure out how this is going to work
+        fileName = 'test%i.pd' % self.activePatch
+        patchInfo = (fileName, '/home/guy/gitrepositories/Radio-PD/patches')
+        return patchInfo
+        #patchList = os.listdir(patchDir)
+        #patch = random.choose(patchList)
+    
+    def activate_patch(self):
+        patch = 'patch' + str(self.activePatch)
+        logFile.log("Turning on DSP in %s" % patch)
+        message = [patch, 'dsp', 1]
+        self.Send(message)
+    
+    def crossfade(self):
+        #fade across to new active patch
+        newPatch = 'patch%i' % self.activePatch
+        oldPatch = 'patch%i' % self.oldPatch
+        logFile.log("Fading over to %s" % patch)
+        
+        newmessage = [newPatch, 'volume', 1]
+        oldmessage = [oldPatch, 'volume', 0]
+        
+        self.Send(newmessage)
+        self.Send(oldmessage)
+        
+        self.pause(10)
+    
+    def kill_old_patch(self):
+        #disconnect old patch from master patch and then del the object
+        if self.oldPatch in self.patches.keys():
+            patch = self.patchNames[self.oldPatch]
+            logFile.log("Stopping %s" % patch)
+            message = [patch, 'dsp', 0]
+            self.Send(message)
+            self.pause(1)
+            message = ['close', patchName]
+            self.Send(message)
+            del(self.patches[self.oldPatch])
+        pass
+    
     def Pd_register(self, data):
         patchNum = data[0]
-        logFile.log(self.name + " Registering:" + str(patchNum))
+        patchName = 'patch%i' % self.activePatch
+        logFile.log(" Registering number %i to %s" % (patchNum, patchName))
         self.patches[self.activePatch] = patchNum
-        reg = 'reg' + str(self.activePatch)
+        reg = 'reg%i' % self.activePatch
         message = [reg, patchNum]
         self.Send(message)
-        
+    
     def PdMessage(self, data):
-        logFile.log(self.name + " Message from PD:" + str(data))
+        logFile.log("Message from PD:" + str(data))
     
     def Error(self, error):
-        logFile.log(self.name + " stderr from PD:" + str(error))
+        logFile.log("stderr from PD:" + str(error))
     
     def PdStarted(self):
-        logFile.log(self.name + " has started")
+        logFile.log("PD has started")
     
     def PdDied(self):
-        logFile.log(self.name + " has died")
+        logFile.log("PD has died")
+    
 
-        
 class ServerDaemon(Daemon):
     
     def run(self, foreground=False):
@@ -158,44 +171,43 @@ class ServerDaemon(Daemon):
         global logFile
 
         #create mixing/streaming patch
-        masterPD = MasterPD(foreground=foreground)
-        masterPD.pause(1)
+        puredata = PureData(debug=foreground)
+        puredata.pause(1)
         #check that the master PD patch is OK
-        if masterPD.Alive:
-            pass
-            #put something in the logFile
+        if puredata.Alive:
+            logFile.log('PureData has started fine')
         else:
-            #PROBLEM HERE!! LOG IT!!
+            logFile.log('Problem starting PureData')
             exit(1)
 
-        masterPD.Send(['dsp', 1])
-
+        puredata.Send(['dsp', 1])
+        
         #start streaming
-        #masterPD.streaming_control("go")
+        #puredata.streaming_control("go")
         
         while True:
             #switch which patch is active
-            masterPD.switch_patch()
+            puredata.switch_patch()
             
             #tell master PD to create the new patch
-            masterPD.create_new_patch()
-
-            masterPD.pause(5)
-
-            masterPD.activate_patch()
-
+            puredata.create_new_patch()
+            
+            puredata.pause(5)
+            
+            puredata.activate_patch()
+            
             #fade over to new patch
-            masterPD.crossfade()
+            puredata.crossfade()
             
             #kill off old patch
-            masterPD.stop_old_patch()
-                
+            puredata.kill_old_patch()
+            
             #sleep for 10 minutes, untill next patch needs to be loaded
-            masterPD.pause(masterPD.playTime)
+            puredata.pause(puredata.playTime)
             
-            
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    
     daemon = ServerDaemon(pidFile)
     
     if len(sys.argv) == 2:
@@ -214,4 +226,4 @@ if __name__ == "__main__":
     else:
         print "usage: %s start|stop|restart|foreground" % sys.argv[0]
         sys.exit(2)
-                
+        
