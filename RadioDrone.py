81d4ad307f0feea9fc5c6c63e745a44c89ee8a5d
Guy John <guy@notesandvolts.com>
2011-01-20 03:36:07 -0800

    Logfile now created in main object, no longer global. changed calls to it, added header method, neatened up some of the logging messages

--- RadioDrone.py
+++ RadioDrone.py
@@ -4,14 +4,10 @@
 import os
 import re
 import sys
-import shutil
 import random
 from Pd import Pd
 from time import time, strftime
-from daemon import Daemon
 
-logFile   = '/var/log/droneServer.log'
-pidFile   = '/var/run/droneServer.pid'
 cfgFile   = 'config.cfg'
 patchDir  = './patches'
 masterDir = './master'
@@ -19,13 +15,13 @@ masterDir = './master'
 class LoggingObj():
 
     def __init__(self, foreground=False):
-        self.logFile    = os.path.join('')
+        self.logFile    = os.path.join('/var/log/radioServer/radioServer.log')
         self.foreground = foreground
-        if not foreground:
+        if not self.foreground:
             self.fileHandle = open(self.logFile, 'w')
 
-    def log(self, logLine):
-        output = "%s  %s" % (self.timeStamp(), logLine)
+    def write(self, logLine):
+        output = "%s   %s\n" % (self.timeStamp(), logLine)
         if self.foreground:
             print output
         else:
@@ -35,12 +31,20 @@ class LoggingObj():
         stamp = strftime("%Y%m%d-%H:%M:%S")
         return stamp
     
+    def header(self):
+        output = "\n*******************\nStartingUp\n*******************\n"
+        if self.foreground:
+            print output
+        else:
+            self.fileHandle.write(output)
+    
 class Config():
     #Class for loading and holding the config data
     
     def __init__(self, cfgFile):
         cfgData = open(cfgFile)
         
+        #TODO: this is scrappy as hell. improve it
         for line in cfgData:
             line = line.rstrip()
             param,val = line.split(':')
@@ -69,7 +73,7 @@ class Config():
             elif param == 'playTime':
                 self.play = val
             else:
-                logFile.log("CFG ERROR: %s is not a known parameter" % param)
+                self.log.write("CfgError:%s isn't a known parameter" % param)
             
         cfgData.close()
     
@@ -86,8 +90,14 @@ class SubPatch():
 class PureData(Pd):
         
     def __init__(self, gui=False):
-        self.patchName   = 'masterPatch.pd'
         
+        self.patch   = 'masterPatch.pd'
+        
+        self.log         = LoggingObj()
+        self.log.header()
+        
+        #TODO: have the config file path passed
+        #      as an argument to the script
         self.config      = Config(cfgFile)
         
         self.active      = 2
@@ -115,25 +125,25 @@ class PureData(Pd):
         
         path             = [patchDir, masterDir]
         
-        Pd.__init__(self, comPort, self.gui, self.patchName, extra=extras, path=path)
+        Pd.__init__(self, comPort, gui, self.patch, extra=extras, path=path)
         
-        logFile.log(self.argLine)
+        self.log.write("starting PD with: %s" % self.argLine)
     
     def check_alive(self):
         if self.Alive:
-            logFile.log('PureData has started fine')
+            self.log.write('PD started fine')
         else:
-            logFile.log('Problem starting PureData')
+            self.log.write('Problem starting PD')
             sys.exit(2)
     
     def check_network(self):
-        logFile.log('Waiting for PD to register the connection')
+        self.log.write('Waiting for PD to register the connection')
         wait = 0
         while not self.connection:
             self.pause(1)
             wait += 1
             if wait > 20:
-                logFile.log('Problem connecting to PD')
+                self.log.write('Problem connecting to PD')
                 sys.exit(2)
     
     def Pd_connection(self, data):
@@ -141,21 +151,20 @@ class PureData(Pd):
         val  = data[1]
         if type == "status":
             if val == "1":
-                logFile.log('Network connection to PD is up')
+                self.log.write('Network connection to PD is up')
                 self.connection = True
     
     def pause(self, pauseLength):
         #pause for a specified number of seconds
         #will keep updating the master patch and sub patches
         #so that network communication still works
-        logFile.log("Pausing for %i seconds" % pauseLength)
         start = time()
         while time() - start < pauseLength:
             self.Update()
     
     def streaming_setup(self):
         #send a message to the streaming controls in the master patch
-        
+        self.log.write("Setting up streaming")
         config     = self.config
         
         password   = config.password
@@ -165,22 +174,22 @@ class PureData(Pd):
         message = ["stream", "password", password]
         self.Send(message)
         
-        logFile.log("HostInfo is %s" % str(hostInfo))
+        self.log.write("HostInfo is %s" % str(hostInfo))
         message = ["stream", "hostinfo", " ".join(hostInfo)]
         self.Send(message)
         
-        logFile.log("Stream Info is %s" % str(settings))
+        self.log.write("Stream Info is %s" % str(settings))
         message = ["stream", "settings", " ".join(settings)]
         self.Send(message)
         
-        logFile.log("Connecting")
+        self.log.write("Attempting connection to Icecast")
         message = ["stream", "connect", 1]
         self.Send(message)
         
     
     def switch_patch(self):
         #change the active patch number
-        #real scrappy, needs to be neater
+        #TODO: real scrappy, needs to be neater
         if self.active == 1:
             self.active = 2
             self.old    = 1
@@ -188,7 +197,7 @@ class PureData(Pd):
             self.active = 1
             self.old    = 2
         name = "patch%i" % self.active
-        logFile.log("Changing active patch to be %s" % name)
+        self.log.write("Changing active patch to be %s" % name)
     
     def create_new_patch(self):
         
@@ -197,13 +206,13 @@ class PureData(Pd):
         #get a random patch from the patch folder
         patch, path = self.get_random_patch()
         name = self.patches[self.active].name
-        logFile.log("Loading new patch for %s" % name)
+        self.log.write("Loading new patch for %s" % name)
         
         #update patch object in active slot
         self.patches[self.active].patch = patch
         self.patches[self.active].path  = path
         
-        logFile.log("New Patch - %s in %s" % (patch, path))
+        self.log.write("New Patch is %s in %s" % (patch, path))
         self.Send(['open', 'path', path])
         self.Send(['open', 'patch', patch])
         
@@ -227,7 +236,6 @@ class PureData(Pd):
         #will need to figure out how this is going to work
 
         current = self.patches[self.old].patch
-        logFile.log("Active patch is %s" % current)
         
         found = False
         while not found:
@@ -239,14 +247,13 @@ class PureData(Pd):
                 if self.fileMatch.search(file):
                     break
         
-            logFile.log("Chosen %s as new patch" % file)
-            logFile.log("Check if %s is %s" % (file, current))
+            self.log.write("Chosen %s as new patch" % file)
             if file != current:
-                logFile.log("Is not the same as the old patch")
+                self.log.write("Is not the same as the old patch")
                 found = True
             else:
-                logFile.log("%s is the same patch as the current one" % file)
-                logFile.log("Will search again")
+                self.log.write("%s is also the current patch" % file)
+                self.log.write("Choosing again.")
         
         patchInfo = (file, dataDir)
         return patchInfo
@@ -256,9 +263,9 @@ class PureData(Pd):
         patch = self.patches[self.active].patch
         path  = self.patches[self.active].path
         
-        logFile.log("***************************************")
-        logFile.log("Problem loading %s from %s" % (patch, path))
-        logFile.log("Unloading patch and starting again")
+        self.log.write("***************************************")
+        self.log.write("Problem loading %s from %s" % (patch, path))
+        self.log.write("Unloading patch and starting again")
         
         message = ['close', patch]
         self.Send(message)
@@ -266,7 +273,7 @@ class PureData(Pd):
     def activate_patch(self):
         #turn on DSP in new patch
         name = self.patches[self.active].name
-        logFile.log("Turning on DSP in %s" % name)
+        self.log.write("Turning on DSP in %s" % name)
         message = [name, 'dsp', 1]
         self.Send(message)
         self.pause(1)
@@ -274,7 +281,7 @@ class PureData(Pd):
     def crossfade(self):
         #fade across to new active patch
         newName = self.patches[self.active].name
-        logFile.log("Fading over to %s" % newName)
+        self.log.write("Fading over to %s" % newName)
         
         message = ['volume', 'fade', self.fadeTime]
         self.Send(message)
@@ -290,7 +297,7 @@ class PureData(Pd):
         name  = self.patches[self.old].name
         if self.patches[self.old].ok:
             patch = self.patches[self.old].patch
-            logFile.log("Stopping %s" % name)
+            self.log.write("Stopping %s" % name)
             
             message = [name, 'dsp', 0]
             self.Send(message)
@@ -304,7 +311,7 @@ class PureData(Pd):
             self.Send(message)
             self.patches[self.old].ok = False
         else:
-            logFile.log("%s doesn't seem to be running currently" % name)
+            self.log.write("%s doesn't seem to be running" % name)
     
     def Pd_register(self, data):
         #Gets the unique number from the PD subPatch
@@ -314,7 +321,7 @@ class PureData(Pd):
         self.patches[self.active].pdNum = pdNum
         self.patches[self.active].ok    = True
         name  = self.patches[self.active].name
-        logFile.log("Registering number %s to %s" % (pdNum, name))
+        self.log.write("Registering number %s to %s" % (pdNum, name))
         
         reg = 'reg%i' % self.active
         message = [reg, pdNum]
@@ -324,38 +331,29 @@ class PureData(Pd):
         self.regWait = False
     
     def PdMessage(self, data):
-        logFile.log("Message from PD:%s" % str(data))
+        self.log.write("Message from PD:%s" % str(data))
     
     def Error(self, error):
         if error[0] == "print:":
-            logFile.log("PD print:%s" % str(error[1:]))
+            self.log.write("Print:%s" % str(error[1:]))
         elif error[0] == "oggcast~:":
-            logFile.log("OggCast:%s" % str(error[1:]))
+            self.log.write("OggCast:%s" % str(error[1:]))
         else:
-            logFile.log("stderr from PD:%s" % str(error))
+            self.log.write("PD:%s" % str(error))
     
     def PdStarted(self):
-        logFile.log("PD has started")
+        self.log.write("PD has started")
     
     def PdDied(self):
-        logFile.log("PD has died")
+        self.log.write("PD has died")
     
     def ComError(self, data):
-        logFile.log("Communication error :%s" % str(error))
+        self.log.write("ComsError:%s" % str(error))
     
 
 def main():
     
-    global logFile
-
-    logFile = LoggingObj()
     
-    logFile.log('\n\n')
-    logFile.log('Radio Drone Starting Up')
-    logFile.log('')
-    
-    #TODO: pass a reference to the logFile to the puredata Object
-
     #create mixing/streaming patch
     puredata = PureData()
     puredata.pause(1)
@@ -399,4 +397,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
