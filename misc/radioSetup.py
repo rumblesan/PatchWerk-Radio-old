8127d7cfbb7838a0ab2443f47266158c7bedb9d4
Guy John <d.guyjohn@gmail.com>
2011-11-14 00:10:39 +0000

    Improving arg parsing of radio setup script

--- misc/radioSetup.py
+++ misc/radioSetup.py
@@ -1,36 +1,78 @@
-#!/bin/sh
+#!/bin/bash
 
 #meant to be run from a directory containing the
 #PatchWerk-Radio and Radio-Patches repositories
 
-if [ -z "$1" ] ; then
-    env=development
-else
-    env=$1
-fi
+usage()
+{
+cat << EOF
+usage: $0 options
 
-#set the variables depending on whether a live or a test setup is wanted
-if [ $env = "master" ] ; then
-    INSTNAME=PatchWerk
-    BRANCH=master
-    CONFIG=config.cfg
-else
-    INSTNAME=TestPatchWerk
-    BRANCH=development
-    CONFIG=testconfig.cfg
+This script sets up PatchWerk Radio
+
+MANDATORY:
+   -d      The PatchWerk Radio repository
+   -p      The Radio Patches repository
+   -c      The configfile
+
+OPTIONS:
+   -h      Show this message
+   -n      The name of the PatchWerk instance (Defaults to TestPatchWerk)
+   -b      The git branch to checkout         (Defaults to master)
+EOF
+}
+
+INSTNAME=TestPatchWerk
+BRANCH=master
+CONFIG=
+APPREPO=
+PATCHREPO=
+
+while getopts "n:b:c:d:p:" OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 1
+            ;;
+        n)
+            INSTNAME=$OPTARG
+            ;;
+        b)
+            BRANCH=$OPTARG
+            ;;
+        c)
+            CONFIG=$OPTARG
+            ;;
+        d)
+            APPREPO=$OPTARG
+            ;;
+        p)
+            PATCHREPO=$OPTARG
+            ;;
+        ?)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z $CONFIG ] || [ -z $APPREPO ] || [ -z $PATCHREPO ]
+then
+    usage
+    exit 1
 fi
 
 BINDIR=/usr/local/sbin/$INSTNAME
-LOGDIR=/var/log/$INSTNAME
 WORKDIR=/var/$INSTNAME
 PATCHDIR=$WORKDIR/patches
 TMPDIR=$WORKDIR/temp
 
 #checkout the correct branches
-cd PatchWerk-Radio
+cd $APPREPO
 sudo -u guy git checkout $BRANCH
 
-cd ../Radio-Patches
+cd $PATCHREPO
 sudo -u guy git checkout master
 
 
@@ -39,14 +81,14 @@ cd ..
 # Clear out the binary dir and then copy the correct files into it
 rm -r $BINDIR
 mkdir -p $BINDIR
-cp -r PatchWerk-Radio/app/*         $BINDIR/
+cp -r $APPREPO/app/*         $BINDIR/
 
 #if the config file exists in the same directory
 #copy it to the bindir
-if [ -f ./$CONFIG ]
+if [ -f $CONFIG ]
 then
     echo using the config file here
-    cp  ./$CONFIG $BINDIR/
+    cp  $CONFIG $BINDIR/
 fi
 
 #give files the correct permissions
@@ -57,7 +99,7 @@ chown -R patchwerk:radio $BINDIR
 # Clear out the patch directory, then copy the new patches back into it
 rm -r $WORKDIR
 mkdir -p $PATCHDIR
-cp -r ./Radio-Patches/* $PATCHDIR/
+cp -r $PATCHREPO/* $PATCHDIR/
 rm -r $PATCHDIR/README
 
 mkdir -p $TMPDIR
